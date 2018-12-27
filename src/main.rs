5c5eb7bd2cc950f214674cb8b78d4368786ef8e8
Esty Thomas <esty@healpay.com>
2018-12-26 23:22:32 -0500

    Pretty calendar

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,74 @@
-extern crate csv;
-extern crate chrono;
+#[macro_use]
+extern crate tera;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
 
 use chrono::{Date, DateTime, Local, Weekday};
 use std::str::FromStr;
+use std::path::Path;
+use std::fs::File;
+use std::io::Write;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 enum Recurrence {
     Daily,
     Weekly(Weekday),
     Monthly
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct AbstractChore {
     name: String,
     recurrence: Recurrence,
     people: Vec<String>
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct ManifestChore {
     name: String,
     day: Weekday,
     person: String
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct Day {
     day: Weekday,
-    date: Date<Local>,
+    date_string: String,
     chores: Vec<ManifestChore>
 }
 
-fn build_calendar(start: Date<Local>) -> Vec<Day> {
+fn build_calendar(start: Date<Local>) -> (Vec<Day>, Vec<Weekday>) {
     let mut month = vec![];
+    let mut header_days = vec![];
     let mut now = start.clone();
     while month.len() < 28 {
         // If this fails, chrono isn't doing its job
         let day = Weekday::from_str(&format!("{}", now.format("%a"))).unwrap();
+        if header_days.len() < 7 {
+            header_days.push(day);
+        }
         let new_day = Day {
             day,
-            date: now,
+            date_string: format!("{}", now.format("%b %d")),
             chores: vec![]
         };
         month.push(new_day);
         now = now.succ();
     }
-    return month
+    (month, header_days)
 }
 
 fn main() {
-    let month = build_calendar(Local::now().date());
-    println!("{:?}", month)
+    let (month, header_days) = build_calendar(Local::now().date());
+    println!("{:?}", month);
+    let tera = compile_templates!("./templates/*");
+    let context = json!({
+        "month": &month,
+        "header_days": &header_days,
+    });
+    let render = tera.render("calendar.html", &context).unwrap();
+    let mut f = File::create("./output.html").unwrap();
+    f.write_all(render.as_bytes()).unwrap();
 }
